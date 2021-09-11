dfb530a5b50d4cdbfd056e7c0f8dbee084d599c2
Igor Marcelo Silva Lima <39357826+igormarcello@users.noreply.github.com>
2021-09-11 15:57:58 -0300

    Trabalho final

---
+++ lib/services/quiz_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quiz_covid19/models/question.dart';
+
+class QuizApi {
+  static Future<List<Question>> fetch() async {
+    try {
+      var url =
+          'https://script.google.com/macros/s/AKfycbzk8mctjJuvVPZ2mZU6mWQLIPiaFo0f-GM6jWhc2C-z4Y_NEpQTwHdy_ac4MS4MEghqBQ/exec';
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        return List<Question>.from(
+            data["questions"].map((x) => Question.fromMap(x)));
+      } else {
+        return List<Question>();
+      }
+    } catch (error) {
+      print(error);
+      return List<Question>();
+    }
+  }
+}
